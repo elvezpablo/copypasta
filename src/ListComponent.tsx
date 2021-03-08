ac0751e56213feeec590281e81526b5fe8dd88c9
elvezpablo <prangel@gmail.com>
2021-03-08 10:14:48 -0800

    popover start

--- src/ListComponent.tsx
+++ src/ListComponent.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import { Popover } from 'react-tiny-popover'
 import { Pill } from './Pill';
 import { cbs } from './data/categories';
 import { useControls } from "leva";
@@ -37,10 +38,37 @@ const List = styled.div`
     padding: 4px;
     display: flex;
     flex-wrap: wrap;
-    max-height: 300px
+    max-height: 300px;
+    min-height: 1rem;
 `;
 
+const PopOver = styled.div`
+    background-color: #FFF;
+    border: 2px solid #CCC;
+    padding: 3px;
+    border-radius: 3px;
+    
+`;
+
+
+const Input = styled.input`
+            background-color: rgb(251, 251, 253);
+    border-color: rgb(210, 212, 220);
+    border-radius: 0.125rem;
+    border-style: solid;
+    border-width: 0.0625rem;
+    box-sizing: border-box;
+    color: rgb(36, 40, 66);
+    font-family: Asap;
+    font-size: 0.938rem;
+    padding: 0.0625rem 0.75rem;
+    appearance: none;
+    height: 2.25rem;
+    width: 100%;
+`
+
 const ListComponent = () => {
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
     const { categories } = useControls({
         categories: {
             value: 40,
@@ -52,10 +80,22 @@ const ListComponent = () => {
         <Container>
             <Header>
                 <Label>categories ({Math.round(categories)})</Label>
-                <MiniButton>...</MiniButton>
+                <Popover
+                    isOpen={isPopoverOpen}
+                    padding={10}
+                    align={'start'}
+                    positions={['left','bottom' ,'top' ]} // preferred positions by priority
+                    content={<PopOver>
+                    <h4>Categories</h4>
+                        <Input placeholder="Type to search" />
+                    </PopOver>}
+                >
+                    <MiniButton onClick={() => {setIsPopoverOpen(!isPopoverOpen)}}>...</MiniButton>
+                </Popover>
+                
             </Header>
             <List>
-                {cbs.slice(0,categories).map(c => (<Pill>{c.fullTitle}</Pill>))}
+                {cbs.slice(0,categories).map(c => (<Pill key={c.fullTitle}>{c.fullTitle}</Pill>))}
             </List>
         </Container>
     )
